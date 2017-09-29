552b525ca91c4696ba87670c6d9777c61eaa0846
Raghu Betina <raghu@firstdraft.com>
2017-09-29 18:33:52 +0000

    Direct association between photos and likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,11 @@
 class Like < ApplicationRecord
   # Direct associations
 
+  belongs_to :photo_with_likes,
+             :class_name => "Photo",
+             :foreign_key => "photo_id",
+             :counter_cache => true
+
   belongs_to :user_who_likes_photos,
              :class_name => "User",
              :foreign_key => "user_id",
