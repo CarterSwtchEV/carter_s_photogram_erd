6ba5a2318d24c56209913a60913f903fbc2f96a6
Raghu Betina <raghu@firstdraft.com>
2017-09-29 18:33:49 +0000

    Direct association between users and likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,11 @@
 class Like < ApplicationRecord
   # Direct associations
 
+  belongs_to :user_who_likes_photos,
+             :class_name => "User",
+             :foreign_key => "user_id",
+             :counter_cache => true
+
   # Indirect associations
 
   # Validations
